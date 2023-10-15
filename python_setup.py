4dad9f3ed19db805fdc08efde12c5204f3691e8c
aar121 <arifrahim12345@gmail.com>
2023-10-15 10:03:42 -0400

    hw

---
+++ python_setup.py
@@ -0,0 +1,17 @@
+print("Hello from inside a file!")
+
+def hello():
+    print("Hi user")
+
+def pack(a, b, c):
+    return[a, b, c]
+
+def eat_lunch(list):
+  if len(list) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(list)):
+      if i == 0:
+        print(f"First I eat {list[0]}")
+      else:
+        print(f"Next I eat {list[i]}")
